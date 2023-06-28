431080a03b425e84b9130a98b971a787351a3cb8
Luigy Leonardo <luigyleonardo23@gmail.com>
2023-06-28 13:53:06 -0500

    ult actualizacion

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
     <MenuNav />
-    <main className='w-full h-[calc(100vh-4rem)] bg-smoke-700 flex flex-col items-center justify-center'>
+    <main className='w-full h-[calc(100vh-4rem)] bg-smoke-800 flex flex-col items-center justify-center'>
       <h1 className='text-slate-50 font-bold uppercase '>Luigy <span className='text-fercho-400'>Leonardo</span> </h1>
       <h2 className='text-slate-50 font-medium mt-2  '>Desarrollador Web | Full-Stack</h2>
       <button className='btn-contact'>Contactame <Left_Arrow /></button>
