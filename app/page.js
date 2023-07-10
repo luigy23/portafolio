beb5d4f04e24f6bdea1a5968c87f3d8edde0a39a
luigy23 <luigyleonardo23@gmail.com>
2023-07-10 15:04:47 -0500

    luces

--- app/page.js
+++ app/page.js
@@ -5,31 +5,39 @@ import Left_Arrow from './Icons/Left_Arrow'
 import Down_Arrow from './Icons/Down_Arrow'
 import MenuNav from './Componentes/MenuNav'
 import Proyectos from './Secciones/Proyectos'
+import logo from '@/public/logoV.svg'
 
 
 export default function Home() {
   return (
-    <>
+    <main className='w-full overflow-x-hidden relative'>
     <MenuNav />
-    <main className='w-full h-screen bg-smoke-800 flex flex-col items-center justify-center'>
+    
+    <section className='w-full min-h-screen bg-smoke-800 flex flex-col items-center justify-center relative overflow-x-hidden'>
+      
+
+      <Image src={logo} width={120} height={120} className='rounded-lg mb-3 object-cover'  />
+
       <h1 className='text-slate-50 font-bold uppercase '>Luigy <span className='text-fercho-400'>Leonardo</span> </h1>
       <h2 className='text-slate-50 font-medium mt-2  '>Desarrollador Web | Full-Stack</h2>
       <button className='btn-contact'>Contactame <Left_Arrow /></button>
       <Down_Arrow className="text-slate-50 w-10  down" />
 
 
-    </main>
-    <section className='w-full pt-4 min-h-screen  bg-smoke-800 flex flex-col items-center justify-center'>
+    </section>
+
+    <section className='w-full pt-4 min-h-screen relative  bg-smoke-800 flex flex-col items-center justify-center'>
       
       <Proyectos />
+      <span className='blob2 '></span>
     </section>
     <section className='w-full h-screen   flex flex-col items-center justify-center'>
       <h1 className='text-slate-50 font-bold uppercase '>Sobre mi</h1>
     </section>
-  
+    <span className='blob '></span>
     
     
-    </>
+    </main>
   )
 }
 
